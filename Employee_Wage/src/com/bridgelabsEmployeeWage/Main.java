0c42d6ee12544b05f18a244787e12774f01afb41
Thejaswi <thejaswirl187@gmail.com>
2022-03-13 20:59:19 +0530

    Welcome Message Added

---
+++ Employee_Wage/src/com/bridgelabsEmployeeWage/Main.java
@@ -0,0 +1,9 @@
+package com.bridgelabsEmployeeWage;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee wage problems");
+    }
+    }
+
---
+++ Employee_Wage/src/com/bridgelabsEmployeeWage/UC1.java
@@ -0,0 +1,18 @@
+package com.bridgelabsEmployeeWage;
+import java.util.Random;
+public class UC1 {
+
+            public static void main (String[] args)
+            {
+                Random r = new Random();
+                int attendance = r.nextInt(2);
+                System.out.println("\nRandom value is: " +attendance);
+
+                if (attendance == 1) {
+                    System.out.println("\nEmployee is Present");
+                } else {
+                    System.out.println("\nEmployee is Absent");
+                }
+
+            }
+        }
